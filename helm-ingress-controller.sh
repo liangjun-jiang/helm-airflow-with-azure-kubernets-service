c43ffb66cd8de99cb5f80040fd85322969507dfa
Liangjun Jiang <jiang@afanta.ai>
2019-06-04 00:21:11 -0500

    now under version control

---
+++ helm-ingress-controller.sh
@@ -0,0 +1,5 @@
+helm install stable/nginx-ingress \
+    --namespace default \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
